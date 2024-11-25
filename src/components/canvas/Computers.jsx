5fa0f806da50729fb92c1e42382effa10efdc31a
John Mathew <jmathew@acilogistix.com>
2024-11-25 22:04:58 +0530

    Guitar model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import {Suspense, useEffect, useState} from 'react';
+import {Canvas} from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+
+import CanvasLoader from '../Loader';
 
 const Computers = () => {
+  const computer = useGLTF('./guitar_house/scene.gltf')
+  
+  
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={1}
+      groundColor="black" />
+      <pointLight intensity={5} />
+      <spotLight
+        position={[-20,50,10]}
+        angle={0.222}
+        penumbra={1}
+        castShadow
+        intensity={9}
+        shadow-mapSize={1024} />
+
+      <primitive
+      object={computer.scene} 
+      scale={0.0111}
+      position={[-1.11, -4.44, 0]}
+      rotation = {[-0.01, -0.2, -0.1]}/>
+    </mesh>
   )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop='demand'
+      shadows
+      camera={{ position: [22, 10, 5], fov: 22}}
+      gl={{preserveDrawingBuffer: true}}
+      >
+        <Suspense fallback = {<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI /2}
+          minPolarAngle={Math.PI/2} />
+          <Computers />
+        </Suspense>
+        <Preload all />
+      </Canvas>
+  )
+}
+
+export default ComputersCanvas;
